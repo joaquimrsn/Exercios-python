08f9ed9e1c36e267ce666a7a441939bb6b203d62
Joaquim Soares <joaquimsoaresnt@gmail.com>
2021-07-03 08:08:12 -0300

    Exercícios Básicos

--- Estrutura sequencial/peso_ideal.py
+++ Estrutura sequencial/peso_ideal.py
@@ -1,4 +1,4 @@
 altura = float(input("Digite a sua altura em metros!"))
 peso_ideal = (72.7*altura) - 58
 
-print("Seu peso ideal é {:.2f} kg".format(peso_ideal))
+print("Seu peso ideal é {} kg",format(peso_ideal))
---
+++ Estrutura sequencial/peso_ideal_homem.py
@@ -0,0 +1,4 @@
+altura = float(input("Digite a sua altura (homem)!"))
+peso_ideal = (72.7*altura)-58
+print("Seu peso ideal é {:.2f}".format(peso_ideal))
+
