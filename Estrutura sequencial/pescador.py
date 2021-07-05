11c828a868b7537a4f95a3c2cc56bfbfecb93f14
Joaquim Soares <joaquimsoaresnt@gmail.com>
2021-07-05 20:42:06 -0300

    exercícios

---
+++ Estrutura sequencial/pescador.py
@@ -0,0 +1,4 @@
+peso = float(input("Digite o quantidade de quilos produzidos!"))
+excesso = peso-50
+multa = excesso*4
+print("A multa é igual a R$ {}".format(multa))
---
+++ Estrutura sequencial/peso_ideal_mulher.py
@@ -0,0 +1,3 @@
+altura = float(input("Digite a sua altura (mulher"))
+peso_ideal = (62.1*altura) - 44.7
+print("Seu peso ideal é {:.2f}".format(peso_ideal))
